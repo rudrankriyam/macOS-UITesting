b5a28ca84e8c1267d6e7d611951992dd967f3da2
Rudrank Riyam <rudrankriyam@gmail.com>
2021-11-29 01:50:38 +0530

    Add build script

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#
+# Download the latest state (`develop` branch) of the iOS SDK https://github.com/Microsoft/AppCenter-SDK-Apple
+# And overwrite the files in the respective location of the Xcode project.
+
+csrutil status
